5ac6842f2e1d406e2fb07ba931a4e2f6a1527650
Samuel Tang <samuel@blocksq.com>
2019-09-04 23:15:24 +0800

    Amend part of the test cases

--- spake2_test.go
+++ spake2_test.go
@@ -21,7 +21,7 @@ type KDFOptions struct {
 	AAD string `yaml:"AAD"`
 }
 
-type TestVector struct {
+type SPAKE2TestVector struct {
 	MHF            MHFOptions `yaml:"mhf"`
 	KDF            KDFOptions `yaml:"kdf"`
 	ClientIdentity string     `yaml:"client_identity"`
@@ -30,6 +30,25 @@ type TestVector struct {
 	X              string     `yaml:"x"`
 	Y              string     `yaml:"y"`
 	Verifier       string     `yaml:"verifier"`
+	MessageA       string     `yaml:"message_a"`
+	MessageB       string     `yaml:"message_b"`
+	Transcript     string     `yaml:"transcript"`
+	HashTranscript string     `yaml:"hash_transcript"`
+	ConfirmationA  string     `yaml:"confirmation_a"`
+	ConfirmationB  string     `yaml:"confirmation_b"`
+	SharedSecret   string     `yaml:"shared_secret"`
+}
+
+type SPAKE2PlusTestVector struct {
+	MHF            MHFOptions `yaml:"mhf"`
+	KDF            KDFOptions `yaml:"kdf"`
+	ClientIdentity string     `yaml:"client_identity"`
+	ServerIdentity string     `yaml:"server_identity"`
+	Password       string     `yaml:"password"`
+	X              string     `yaml:"x"`
+	Y              string     `yaml:"y"`
+	VerifierW0     string     `yaml:"verifier_w0"`
+	VerifierL      string     `yaml:"verifier_l"`
 	MessageA       string     `yaml:"message_a"`
 	MessageB       string     `yaml:"message_b"`
 	Transcript     string     `yaml:"transcript"`
@@ -290,7 +309,7 @@ func TestSPAKE2Vectors(t *testing.T) {
 	yamlFile, err := ioutil.ReadFile("./test_vectors/spake2_ed25519_sha256_hkdf_hmac_scrypt.yml")
 	assert.NoError(t, err)
 
-	var testVectors []TestVector
+	var testVectors []SPAKE2TestVector
 	err = yaml.Unmarshal([]byte(yamlFile), &testVectors)
 	assert.NoError(t, err)
 
@@ -609,4 +628,131 @@ func TestSPAKE2PlusWithWrongServerIdentity(t *testing.T) {
 	assert.Error(t, err)
 }
 
-func TestSPAKE2PlusVectors(t *testing.T) {}
+func TestSPAKE2PlusVectors(t *testing.T) {
+	yamlFile, err := ioutil.ReadFile("./test_vectors/spake2plus_ed25519_sha256_hkdf_hmac_scrypt.yml")
+	assert.NoError(t, err)
+
+	var testVectors []SPAKE2PlusTestVector
+	err = yaml.Unmarshal([]byte(yamlFile), &testVectors)
+	assert.NoError(t, err)
+
+	for _, testVector := range testVectors {
+		// Defines the cipher suite
+		suite := Ed25519Sha256HkdfHmacScrypt(
+			Scrypt(testVector.MHF.N, testVector.MHF.R, testVector.MHF.P),
+		)
+
+		clientIdentity := []byte(testVector.ClientIdentity)
+		serverIdentity := []byte(testVector.ServerIdentity)
+		password := []byte(testVector.Password)
+		salt := []byte(testVector.MHF.Salt)
+		aad := []byte{}
+
+		xHex, err := hex.DecodeString(testVector.X)
+		if !assert.NoError(t, err) {
+			return
+		}
+		x, err := suite.Curve().NewScalar(xHex)
+		if !assert.NoError(t, err) {
+			return
+		}
+		yHex, err := hex.DecodeString(testVector.Y)
+		if !assert.NoError(t, err) {
+			return
+		}
+		y, err := suite.Curve().NewScalar(yHex)
+		if !assert.NoError(t, err) {
+			return
+		}
+		expectedVerifierW0, err := hex.DecodeString(testVector.VerifierW0)
+		if !assert.NoError(t, err) {
+			return
+		}
+		expectedVerifierL, err := hex.DecodeString(testVector.VerifierL)
+		if !assert.NoError(t, err) {
+			return
+		}
+		expectedMessageA, err := hex.DecodeString(testVector.MessageA)
+		if !assert.NoError(t, err) {
+			return
+		}
+		expectedMessageB, err := hex.DecodeString(testVector.MessageB)
+		if !assert.NoError(t, err) {
+			return
+		}
+		// expectedTranscript, err := hex.DecodeString(testVector.Transcript)
+		// if !assert.NoError(t, err) {
+		// 	return
+		// }
+		// expectedHashTranscript, err := hex.DecodeString(testVector.HashTranscript)
+		// if !assert.NoError(t, err) {
+		// 	return
+		// }
+		expectedConfirmationA, err := hex.DecodeString(testVector.ConfirmationA)
+		if !assert.NoError(t, err) {
+			return
+		}
+		expectedConfirmationB, err := hex.DecodeString(testVector.ConfirmationB)
+		if !assert.NoError(t, err) {
+			return
+		}
+		expectedSharedSecret, err := hex.DecodeString(testVector.SharedSecret)
+		if !assert.NoError(t, err) {
+			return
+		}
+
+		// Creates a SPAKE2+ instance
+		s, err := NewSPAKE2Plus(suite)
+		if !assert.NoError(t, err) {
+			return
+		}
+
+		verifierW0, verifierL, err := s.ComputeVerifier(password, salt, clientIdentity, serverIdentity)
+		if !assert.Equal(t, expectedVerifierW0, verifierW0) {
+			return
+		}
+		if !assert.Equal(t, expectedVerifierL, verifierL) {
+			return
+		}
+
+		// Creates a SPAKE2 client and a SPAKE2 server.
+		stateA, messageA, err := s.startClient(clientIdentity, serverIdentity, password, salt, aad, x)
+		if !assert.NoError(t, err) || !assert.Equal(t, expectedMessageA, messageA) {
+			return
+		}
+		stateB, messageB, err := s.startServer(clientIdentity, serverIdentity, verifierW0, verifierL, aad, y)
+		if !assert.NoError(t, err) || !assert.Equal(t, expectedMessageB, messageB) {
+			return
+		}
+
+		// A and B verify the incoming message from each other.
+		sharedSecretA, err := stateA.Finish(messageB)
+		if !assert.NoError(t, err) {
+			return
+		}
+		sharedSecretB, err := stateB.Finish(messageA)
+		if !assert.NoError(t, err) {
+			return
+		}
+
+		// A and B verify the confirmation message from each other.
+		confirmationA := sharedSecretA.GetConfirmation()
+		confirmationB := sharedSecretB.GetConfirmation()
+		if !assert.Equal(t, expectedConfirmationA, confirmationA) || !assert.Equal(t, expectedConfirmationB, confirmationB) {
+			return
+		}
+
+		err = sharedSecretA.Verify(confirmationB)
+		if !assert.NoError(t, err) {
+			return
+		}
+		err = sharedSecretB.Verify(confirmationA)
+		if !assert.NoError(t, err) {
+			return
+		}
+
+		// A and B have a common shared secret.
+		assert.Equal(t, sharedSecretA.Bytes(), sharedSecretB.Bytes())
+		assert.Equal(t, expectedSharedSecret, sharedSecretA.Bytes())
+	}
+}
