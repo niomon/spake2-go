ef21ad4675e3f6243630f08e7440c61de5615df6
Harry Lee Chak Chiu <harrier@blocksq.com>
2019-09-09 11:53:51 +0800

    Add salt generation utils

--- utils.go
+++ utils.go
@@ -2,6 +2,7 @@ package spake2
 
 import (
 	"bytes"
+	"crypto/rand"
 	"encoding/binary"
 )
 
@@ -28,4 +29,14 @@ func padScalarBytes(scBytes []byte, padLen int) []byte {
 func appendLenAndContent(b *bytes.Buffer, input []byte) {
 	binary.Write(b, binary.LittleEndian, uint64(len(input)))
 	b.Write(input)
+}
+
+// GenerateSalt is a utility function that generate a cryptographic random salt with given length.
+func GenerateSalt(len uint) ([]byte, error) {
+	buffer := make([]byte, len)
+	_, err := rand.Read(buffer)
+	if err != nil {
+		return nil, err
+	}
+	return buffer, nil
 }
